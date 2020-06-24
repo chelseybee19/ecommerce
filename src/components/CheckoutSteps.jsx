d51bb3ec21244f3db60f9b571fff3233126bdd4b
Chelsey Grice <chelseygrice92@gmail.com>
2020-06-24 17:04:27 +0200

    css order page

--- src/components/CheckoutSteps.jsx
+++ src/components/CheckoutSteps.jsx
@@ -4,7 +4,7 @@ function CheckoutSteps(props) {
     <div className={props.step1 ? 'active' : ''} >Signin</div>
     <div className={props.step2 ? 'active' : ''} >Shipping</div>
     <div className={props.step3 ? 'active' : ''} >Payment</div>
-    <div className={props.step4 ? 'active' : ''} >Place Order</div>
+    <div className={props.step4 ? 'active' : ''} >Order Complete</div>
   </div>
 }
 

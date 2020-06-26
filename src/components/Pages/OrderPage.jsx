e2932f455f15523545bde598f10634ecff2ca168
Chelsey Grice <chelseygrice92@gmail.com>
2020-06-26 10:42:39 +0200

    css

--- src/components/Pages/OrderPage.jsx
+++ src/components/Pages/OrderPage.jsx
@@ -4,7 +4,7 @@ import CheckoutSteps from '../CheckoutSteps';
 function OrderPage(props) {
     return <div>
         <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
-        <div className="Order-box">
+        <div className="order-box">
             <h1>Your Order is complete!</h1>
         </div>
     </div>
